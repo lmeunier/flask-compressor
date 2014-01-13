a3db9ed0c1f8515f2597303f2f32fc466f72a9bd
Laurent Meunier <laurent@deltalima.net>
2014-01-13 12:29:31 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Compressor',
-    version='0.1.1',
+    version='0.2.0',
     description='Compress your CSS and JS files.',
     long_description=__doc__,
     author='Laurent Meunier',
